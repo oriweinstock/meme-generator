b4b3e09bab46e2fe8f6ffc73cd16a06110bcbd17
dig-ana-soft <73099315+dig-ana-soft@users.noreply.github.com>
2020-12-09 13:16:51 +0200

    2nd stage. Multi line/image support, click selects, text controls #1

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -12,31 +12,78 @@ function onControllerInit() {
     renderCanvas(gCurrMeme);
 }
 
-function renderCanvas(meme) {
+function renderCanvas() {
     var img = new Image();
-    img.src = `img/${meme.imgId}.jpg`
+    img.src = `img/${gCurrMeme.imgId}.jpg`
     img.onload = () => {
         gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
-        renderCanvasLine(meme.lines[0]);
+        // renderCanvasLine(gCurrMeme.lines[0]);
+        renderCanvasLines();
     }
 }
 
+var renderCanvasLines = () => {
+    gCurrMeme.lines.forEach(line => renderCanvasLine(line));
+}
 function renderCanvasLine(line) {
-    gCtx.lineWidth = 4;
-    gCtx.fillStyle = 'white';
-    gCtx.strokeStyle = 'black';
-    gCtx.font = 'normal 900 60px impact';
+    gCtx.lineWidth = line.strokeWidth;
+    gCtx.fillStyle = line.fillColor;
+    gCtx.strokeStyle = line.strokeColor;
+    // gCtx.font = 'normal 900 60px impact';
+    gCtx.font = `normal 900 ${line.size}px ${line.font}`;
     gCtx.fillText(line.txt, line.pos.x, line.pos.y);
     gCtx.strokeText(line.txt, line.pos.x, line.pos.y)
 }
 
-function onTextChange(txt) {
-    setMemeLine(gCurrMeme, gLineIdx, txt);
+function onImageClick(img) {
+    // console.log('img selected:', img.dataset.imgId)
+    setMemeImage(+img.dataset.imgId);
     renderCanvas(gCurrMeme);
 }
 
+function onTextChange(txt) {
+    setMemeLine(gLineIdx, txt);
+    renderCanvas();
+}
+
+function onFontSelect(fontName) {
+    console.log(fontName);
+    setLineFont(gLineIdx, fontName);
+    renderCanvas();
+}
+
+function onFontSize(diff) {
+    changeFontSize(gLineIdx, +diff)
+    renderCanvas();
+}
+
+function onMoveLine(diffX, diffY) {
+    moveLine(gLineIdx, diffX, diffY);
+    renderCanvas();
+}
 function _clearInputs() {
     // TODO: qsALL => forEach
     var elInput = document.querySelector('#lineText');
     elInput.value = '';
+}
+
+function onCanvasClick(ev) {
+    var { offsetX, offsetY } = ev;
+
+    var clickedLine = gCurrMeme.lines.findIndex((line) => {
+        return offsetX >= line.pos.x && offsetX <= line.pos.x + line.pos.xEnd
+            && offsetY <= line.pos.y && offsetY >= line.pos.y - line.size
+    });
+    if (clickedLine > -1) gLineIdx = clickedLine;
+    _renderCurrLineLabel();
+}
+
+function _renderCurrLineLabel() {
+    document.querySelector('#currLine').value = gLineIdx;
+}
+
+function highLightLine(line = gLineIdx) {
+    gCtx.beginPath();
+    gCtx.rect(50, 50, 150, 150);
+    gCtx.stroke();
 }
--- js/meme-service.js
+++ js/meme-service.js
@@ -12,15 +12,49 @@ var gCurrMeme = {
     lines: [
         {
             txt: 'Hello Meme',
-            pos: {x: 100, y:200},
-            size: 20,
+            font: 'impact',
+            pos: {x: 100, y:200, xEnd: 300},
+            size: 40,
             align: 'center',
-            color: '#FFFFFF'
+            strokeWidth: 3,
+            strokeColor: '#000000',
+            fillColor: '#FFFFFF'
+        },
+        {
+            txt: 'Second LINE!!',
+            font: 'impact',
+            pos: {x: 200, y:400, xEnd: 400},
+            size: 50,
+            align: 'center',
+            strokeWidth: 3,
+            strokeColor: '#000000',
+            fillColor: '#FFAAFF'
         }
     ]
 };
 
-function setMemeLine(meme, lineIdx = 0, txt) { 
-    meme.lines[lineIdx].txt = txt;
+function setMemeLine(lineIdx = 0, txt) { 
+    gCurrMeme.lines[lineIdx].txt = txt;
 }
 
+function getLineEnd(value) {
+    console.log(value)
+    return 100;
+}
+function setMemeImage(imgId) {
+    gCurrMeme.imgId = imgId;
+}
+
+function setLineFont(lineIdx, fontName) {
+    gCurrMeme.lines[lineIdx].font = fontName;
+}
+
+function moveLine(lineIdx, diffX, diffY) {
+    gCurrMeme.lines[lineIdx].pos.x += diffX;
+    gCurrMeme.lines[lineIdx].pos.y += diffY;
+}
+
+function changeFontSize(lineIdx = 0, diff) {
+    console.log(diff)
+    gCurrMeme.lines[lineIdx].size += diff;
+}
